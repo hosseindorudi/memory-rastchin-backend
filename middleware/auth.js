f701d0d6ec9566678f5872ac311d5dda19e7c68e
hosseindorudi <hdorudi@gmail.com>
2023-07-31 16:45:15 +0330

    env file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,13 @@
 import jwt from "jsonwebtoken";
-
-const secret = "test";
+import dotenv from "dotenv";
+dotenv.config();
 
 const auth = async (req, res, next) => {
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret) {
+    return res.status(401).json({ message: "notAuthorize" });
+  }
   try {
     const token = req.headers.authorization.split(" ")[1];
     const isCustomAuth = token.length < 500;
