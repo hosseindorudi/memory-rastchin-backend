0f23027aaeb975d86fc161d32fdfe5289be9d880
hosseindorudi <hdorudi@gmail.com>
2023-08-01 08:48:06 +0330

    asd

--- index.js
+++ index.js
@@ -19,17 +19,20 @@ app.use(function (req, res, next) {
   // res.header("Access-Control-Allow-Origin", "*");
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   next();
-})
+});
 
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
@@ -39,14 +42,10 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL)
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running`)
-    )
-  )
+  .then(() => app.listen(PORT, () => console.log(`Server Running`)))
   .catch((error) => {
     console.log(`${error} did not connect`);
     process.exit(1);
   });
 
-// mongoose.set('useFindAndModify', false);
+export default app;
