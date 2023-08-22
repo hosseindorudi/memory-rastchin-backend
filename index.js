4c4bd50297c8d9491de013691db154ed5879e1e0
hosseindorudi <hdorudi@gmail.com>
2023-08-22 08:48:32 +0330

    co

--- index.js
+++ index.js
@@ -10,17 +10,21 @@ const userRouter = require("./routes/user.js");
 const app = express();
 
 var corsOptions = {
-  origin: ["http://localhost:3000", "https://hd-memories.netlify.app/", "https://hd-memories.netlify.app/*"],
+  origin: [
+    "http://localhost:3000",
+    "https://hd-memories.netlify.app/",
+    "https://hd-memories.netlify.app/*",
+  ],
   optionsSuccessStatus: 200, // For legacy browser support
   credentials: true,
 };
-
 
 app.all("*", function (req, res, next) {
   let origin = req.headers.origin;
   if (cors.origin.indexOf(origin) >= 0) {
     res.header("Access-Control-Allow-Origin", origin);
   }
+  res.set("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -32,7 +36,6 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(corsOptions));
 app.use(helmet());
-
 
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
