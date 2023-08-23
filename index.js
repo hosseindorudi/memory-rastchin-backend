8726432ed5e9ee705d9c985fea186ecbf07dde6e
hosseindorudi <hdorudi@gmail.com>
2023-08-23 12:45:28 +0330

    try

--- index.js
+++ index.js
@@ -13,24 +13,21 @@ var corsOptions = {
   origin: [
     "http://localhost:3000",
     "https://hd-memories.netlify.app/",
+    "https://hd-memories.netlify.app/posts",
     "https://hd-memories.netlify.app/*",
   ],
   optionsSuccessStatus: 200, // For legacy browser support
   credentials: true,
 };
 
-// app.all("*", function (req, res, next) {
-//   let origin = req.headers.origin;
-//   if (cors.origin.indexOf(origin) >= 0) {
-//     res.header("Access-Control-Allow-Origin", origin);
-//   }
-//   res.set("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+app.all("*", function (req, res, next) {
+  // let origin = req.headers.origin;
+  // if (cors.origin.indexOf(origin) >= 0) {
+  res.header("Access-Control-Allow-Origin", "*");
+  // }
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
